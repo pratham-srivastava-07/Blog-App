cf7b350d9a1822768b986992ae5dec69cf1c45b4
Krish <prathamrajsri2003@gmail.com>
2024-05-06 18:37:27 +0530

    progress

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
+import { Button } from './ui/button'
 
 export default function Navbar() {
   return (
-    <nav className='flex'>
+    <nav className='flex items-center justify-between'>
         <div className="group">
             <Link href="/" className='text-2xl font-bold'>MyBlog</Link>
             <div className='h-1 w-0 group-hover:w-full transition-all bg-blue-500'></div>
         </div>
+       <Link href="/signin"><Button variant="outline">Login</Button></Link>
     </nav>
   )
 }
