a22b5c662134d2bd2c92224e8d624aa5edfa1ec0
Krish <prathamrajsri2003@gmail.com>
2024-05-19 00:20:45 +0530

    ..

---
+++ providers/SessionProvider.tsx
@@ -0,0 +1,30 @@
+import { createBrowserClient } from "@supabase/ssr"
+import React, { useEffect } from "react"
+import {useUser} from "@/lib/store/user"
+
+
+
+
+export default function SessionProvider() {
+    const setUser = useUser((state: { setUser: any })=> state.setUser)
+
+    const supabase = createBrowserClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+
+    const readUserSession = async () => {
+        const {data } = await supabase.auth.getSession()
+        setUser(data.session?.user)
+    }
+
+    useEffect(()=> {
+        readUserSession();
+    }, [])
+
+    return (
+        <div>
+
+        </div>
+    )
+}
