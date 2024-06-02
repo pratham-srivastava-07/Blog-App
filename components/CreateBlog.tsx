a721ca05f2ed270f0c0efd3c0781adeceaabdc0f
Krish <prathamrajsri2003@gmail.com>
2024-06-02 14:03:56 +0530

    changes

--- components/CreateBlog.tsx
+++ components/CreateBlog.tsx
@@ -1,8 +1,9 @@
 "use client"
-
+import { useState } from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
+import {BsSave} from "react-icons/bs"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -16,9 +17,9 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { toast } from "@/components/ui/use-toast"
-import { EyeOpenIcon } from "@radix-ui/react-icons"
+import { EyeOpenIcon, Pencil1Icon } from "@radix-ui/react-icons"
 import { Switch } from "@/components/ui/switch"
-import { RocketIcon, StarIcon } from "lucide-react"
+import { PencilIcon, RocketIcon, StarIcon } from "lucide-react"
 
 const FormSchema = z.object({
   username: z.string().min(2, {
@@ -27,6 +28,9 @@ const FormSchema = z.object({
 })
 
 export default function CreateBlog() {
+
+  const [ isPreview, setPreview] = useState(false)
+
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
@@ -50,10 +54,29 @@ export default function CreateBlog() {
       <form 
       onSubmit={form.handleSubmit(onSubmit)} 
       className="w-full rounded-md space-y-6">
-        <div className="p-5 flex gap-5 items-center flex-wrap">
-           <span role="button" tabIndex={0} className="flex items-center gap-1 bg-zinc-600 p-2 rounded-md hover:ring-2 hover:ring-zinc-400 transition-all">
-            <EyeOpenIcon/>
+        <div className="px-5 flex gap-5 items-center flex-wrap justify-between">
+          <div className="flex items-center gap-7">
+           <span 
+           role="button" 
+           tabIndex={0} 
+           onClick={()=>setPreview(!isPreview)}
+           className="flex items-center gap-1 bg-zinc-600 p-2 rounded-md hover:ring-2 hover:ring-zinc-400 transition-all">
+           {isPreview ? 
+           
+            (<>
+            <Pencil1Icon/>
+            Edit
+           </>
+          )
+          :
+          (
+             <>
+          <EyeOpenIcon/>
             Preview
+           </>
+          )
+          }
+            
             </span>
             <FormField
           control={form.control}
@@ -89,6 +112,11 @@ export default function CreateBlog() {
             </FormItem>
           )}
         />
+        </div>
+        <Button className="flex items-cennter gap-2">
+          <BsSave/>
+          Save
+          </Button>
         </div>
         <FormField
           control={form.control}
